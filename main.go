bfbc5c447dc709ae714df889c16c7c1b7c99a5dd
Aleksei Kornev <aleksei.kornev@gmail.com>
2016-01-01 07:44:54 +0300

    Add telegramm bot api library and make simple example

--- main.go
+++ main.go
@@ -1,29 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"teadiller/Godeps/_workspace/src/github.com/parnurzeal/gorequest"
+    "log"
+    "os"
+    "teadiller/Godeps/_workspace/src/github.com/Syfaro/telegram-bot-api"
 )
 
-const TELEGRAMM_URL_PATTERN = "https://api.telegram.org/bot%s/"
-const BOT_URL = ""
-
 func main() {
-	request := gorequest.New()
-	fmt.Println("register bot...")
-	url := fmt.Sprintf(TELEGRAMM_URL_PATTERN, os.Getenv("TELEGRAMM_TOKEN"))
-	resp, _, _ := request.Get(fmt.Sprint("%ssetWebhook?url=%s", url, BOT_URL)).End()
-	if resp.Status == "200" {
-		fmt.Println("listening...")
-		err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
+    bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAMM_TOKEN"))
+    if err != nil {
+        panic(err)
+    }
 
-func hook(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "Got it")
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates, err := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+    }
 }
