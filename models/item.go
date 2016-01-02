96bc632086d9a116868bb3e8ec42a33c624d87b5
Aleksei Kornev <aleksei.kornev@gmail.com>
2016-01-02 08:35:21 +0300

    FIX #6 Create domain models for teabot

---
+++ models/item.go
@@ -0,0 +1,19 @@
+package models
+
+type ItemType int
+
+const (
+    CountItem ItemType = iota
+    VolumeItem ItemType = iota
+)
+
+type Item struct {
+    Id string
+    Name string
+    Desc string
+    PhotoPath string
+    Tags []string
+    Type ItemType
+    Count float64
+    AdditionalFields map[string]string
+}
