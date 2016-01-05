4424d9f3cf611aeeb54b935b78918f92831dbb95
Sevak Avetisyan <sevak.avet@gmail.com>
2016-01-06 02:26:55 +0300

    Implement read/write to/from excel

--- models/order.go
+++ models/order.go
@@ -1,40 +1,115 @@
 package models
 
-type DeliveryType int
-
-const (
-    SelfService DeliveryType = iota
-    RussianPostOffice DeliveryType = iota
+import (
+	"time"
+	"path/filepath"
+	"github.com/tealeg/xlsx"
+	"fmt"
+	"errors"
 )
 
-type OrderStatus int
+type DeliveryType string
+type OrderStatus string
 
 const (
-    New OrderStatus = iota
-    Paid OrderStatus = iota
-    Processing OrderStatus = iota
-    Shipped OrderStatus = iota
-    Delivered OrderStatus = iota
-    Done OrderStatus = iota
-    Cancelled OrderStatus = iota
+	SelfService DeliveryType = "Self-service"
+	RussianPostOffice DeliveryType = "Russian Post Office"
+)
+
+const (
+	New OrderStatus = "New"
+	Paid OrderStatus = "Paid"
+	Processing OrderStatus = "Processing"
+	Shipped OrderStatus = "Shipped"
+	Delivered OrderStatus = "Delivered"
+	Done OrderStatus = "Done"
+	Cancelled OrderStatus = "Cancelled"
 )
 
 type Buyer struct {
-    Name string
+	Name string
 }
 
 type Delivery struct {
-    DeliveryType DeliveryType
-    Address string // optional 
+	DeliveryType DeliveryType
+	Address      string // optional
 }
 
 type Order struct {
-    Id string
-    ItemId string
-    Buyer Buyer
-    Delivery Delivery
-    Datetime uint64
-    Status OrderStatus
-    PaypallToken string
-    Comment string // comments from seller
+	Id           string
+	ItemId       string
+	Buyer        Buyer
+	Delivery     Delivery
+	Datetime     time.Time
+	Status       OrderStatus
+	PaypallToken string
+	Comment      string // comments from seller
+}
+
+func (p DeliveryType) Parse(deliveryType string) DeliveryType {
+	switch deliveryType {
+	case "Self-service":
+		return SelfService
+	case "Russian Post Office":
+		return RussianPostOffice
+	}
+	return SelfService
+}
+
+func (p DeliveryType) String() string {
+	return string(p)
+}
+
+func (p OrderStatus) String() string {
+	return string(p)
+}
+
+func (p OrderStatus) Parse(code string) OrderStatus {
+	switch code {
+	case "New":
+		return New
+	case "Paid":
+		return Paid
+	case "Processing":
+		return Processing
+	case "Shipped":
+		return Shipped
+	case "Delivered":
+		return Delivered
+	case "Done":
+		return Done
+	case "Cancelled":
+		return Cancelled
+	}
+
+	return New
+}
+
+func (order Order) Save(filepath string) {
+	file, _ := xlsx.OpenFile(filepath)
+
+	sheet, _ := getSheetByName(file, "orders")
+	row := sheet.AddRow();
+
+	row.AddCell().SetString(order.Id)
+	row.AddCell().SetString(order.ItemId)
+	row.AddCell().SetString(order.Buyer.Name)
+	row.AddCell().SetString(order.Delivery.DeliveryType.String())
+	row.AddCell().SetString(order.Delivery.Address)
+	row.AddCell().SetString(fmt.Sprint(order.Datetime.Format(time.ANSIC)))
+	row.AddCell().SetString(order.Status.String())
+	row.AddCell().SetString(order.PaypallToken)
+	row.AddCell().SetString(order.Comment)
+
+	file.Save(filepath)
+}
+
+func getSheetByName(file *xlsx.File, sheetName string) (*xlsx.Sheet, error) {
+	for _, sheet := range file.Sheets {
+		if sheet.Name == "orders" {
+			return sheet, nil
+		}
+	}
+
+	return nil, errors.New("Sheet with name = " + sheetName + " not found.")
 }
